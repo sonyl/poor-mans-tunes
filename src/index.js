0bc9be3ec035ec19662fa82a4b7689475aba427d
Gerhard Dickescheid <gdd@eacg.de>
2017-01-02 14:48:16 +0100

    added redux DevTools

--- src/index.js
+++ src/index.js
@@ -2,30 +2,36 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import createLogger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 import reducer from './reducers';
 import Main from './containers/Main';
+import DevTools from './containers/DevTools';
 
 import 'grommet/scss/vanilla/index';
 
-const middleware = [ thunk ];
-if (process.env.NODE_ENV !== 'production') {
-    middleware.push(createLogger());
-}
-
+// const store = createStore(
+//     reducer,
+//     applyMiddleware(...middleware)
+// );
 const store = createStore(
     reducer,
-    applyMiddleware(...middleware)
+    compose(
+        applyMiddleware(thunk, createLogger()),
+        DevTools.instrument()
+    )
 );
 
 
 console.log('Store:', store);
 render(
     <Provider store={store}>
-        <Main />
+        <div>
+            <Main />
+            <DevTools />
+        </div>
     </Provider>, document.getElementById('app')
 );
